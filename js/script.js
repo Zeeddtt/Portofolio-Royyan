72f6f266ad032c8d499353c5a9c7739f874d5a65
Zeeddtt <savana.cell152@gmail.com>
2024-08-22 12:39:04 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ====================Typing Animation ==================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","","Math Consultant","","Ordinary Student","","MEH"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
